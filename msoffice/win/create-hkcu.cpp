c127fac92fca077181307393f9fff6f28f8152d3
Tino Didriksen <mail@tinodidriksen.com>
2015-04-11 14:36:50 +0000

    Fix propagator
    git-svn-id: http://svn.tinodidriksen.com/tdc/oss/spellers@3 52bd5d93-d692-4aa0-9d28-25f7cffc0c32

--- msoffice/win/create-hkcu.cpp
+++ msoffice/win/create-hkcu.cpp
@@ -1,45 +1,87 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <cctype>
+#include <map>
 #include <stdint.h>
 
 #include <winsdkver.h>
 #include <SDKDDKVer.h>
 #include <windows.h>
 
+// Not allowed to take the address of main, so a function that we can take the address of
+std::wstring get_path() {
+	MEMORY_BASIC_INFORMATION mbiInfo = { 0 };
+	std::wstring path(MAX_PATH + 1, 0);
+	if (VirtualQuery(get_path, &mbiInfo, sizeof(mbiInfo))) {
+		GetModuleFileName((HMODULE)(mbiInfo.AllocationBase), &path[0], MAX_PATH);
+	}
+	if (path[0] == 0) {
+		GetModuleFileName(GetModuleHandle(L"create-hkcu.exe"), &path[0], MAX_PATH);
+	}
+	while (!path.empty() && path.back() != '/' && path.back() != '\\') {
+		path.pop_back();
+	}
+	return path;
+}
+
+std::string trim(std::string str) {
+	while (!str.empty() && std::isspace(str[str.size() - 1])) {
+		str.resize(str.size() - 1);
+	}
+	while (!str.empty() && std::isspace(str[0])) {
+		str.erase(str.begin());
+	}
+	return str;
+}
+
 int main() {
-	std::string buf(1024, 0);
-	uint32_t len = buf.size();
+	std::map<std::string, std::string> conf;
+
+	std::wstring path = get_path();
+
+	std::string line;
+	std::ifstream inif(path + L"speller.ini", std::ios::binary);
+	while (std::getline(inif, line)) {
+		line = trim(line);
+		if (line.empty() || line[0] == '#') {
+			continue;
+		}
+
+		size_t eqpos = line.find('=');
+		std::string key = trim(line.substr(0, eqpos));
+		std::string value = trim(line.substr(eqpos + 1));
+
+		conf[key] = value;
+	}
+
+	if (conf.empty() || !conf.count("LOCALE")) {
+		return -1;
+	}
+
+	std::string buf8("Software\\Microsoft\\Shared Tools\\Proofing Tools\\1.0\\Override\\");
+	buf8.append(conf["LOCALE"]);
+
 	HKEY key;
 	LONG rv = 0;
 
-	if ((rv = RegOpenKeyExA(HKEY_LOCAL_MACHINE, "Software\\Microsoft\\Active Setup\\Installed Components\\{A3AC5A53-231B-44A2-A28B-E59CCA979FB0}", 0, KEY_READ|KEY_WOW64_32KEY, &key)) != ERROR_SUCCESS
-		&& (rv = RegOpenKeyExA(HKEY_LOCAL_MACHINE, "Software\\Microsoft\\Active Setup\\Installed Components\\{A3AC5A53-231B-44A2-A28B-E59CCA979FB0}", 0, KEY_READ|KEY_WOW64_64KEY, &key)) != ERROR_SUCCESS) {
-		std::cerr << "Error: Could not open HKLM\\Software\\Microsoft\\Active Setup\\Installed Components\\{A3AC5A53-231B-44A2-A28B-E59CCA979FB0} for reading: " << rv << std::endl;
-		return -rv;
-	}
-	if ((rv = RegQueryValueExA(key, "StubPath", 0, 0, reinterpret_cast<LPBYTE>(&buf[0]), reinterpret_cast<LPDWORD>(&len))) != ERROR_SUCCESS) {
-		std::cerr << "Error: Could not read StubPath: " << rv << std::endl;
-		return -rv;
-	}
-	RegCloseKey(key);
-
-	if ((rv = RegCreateKeyExA(HKEY_CURRENT_USER, "Software\\Microsoft\\Shared Tools\\Proofing Tools\\1.0\\Override\\kl-GL", 0, 0, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, 0, &key, 0)) != ERROR_SUCCESS) {
-		std::cerr << "Error: Could not create HKCU\\Software\\Microsoft\\Shared Tools\\Proofing Tools\\1.0\\Override\\kl-GL: " << rv << std::endl;
+	if ((rv = RegCreateKeyExA(HKEY_CURRENT_USER, buf8.c_str(), 0, 0, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, 0, &key, 0)) != ERROR_SUCCESS) {
+		std::cerr << "Error: Could not create " << buf8 << ": " << rv << std::endl;
 		return -rv;
 	}
 
-	// Strip create_hkcu.exe\0 from the string
-	buf.resize(len-1-15);
-
-	std::string data;
-	if (data.assign(buf + "kalspell.dll").empty() || (rv = RegSetValueExA(key, "DLL", 0, REG_SZ, reinterpret_cast<const BYTE*>(data.c_str()), data.size()+1)) != ERROR_SUCCESS) {
+	std::wstring data;
+	if (data.assign(path + L"office32.dll").empty()
+		|| (rv = RegSetValueExW(key, L"DLL", 0, REG_SZ, reinterpret_cast<const BYTE*>(data.c_str()), (data.size()+1)*sizeof(data[0]))) != ERROR_SUCCESS) {
 		std::cerr << "Error: Could not write DLL: " << rv << std::endl;
 	}
-	if (data.assign(buf + "kalspell64.dll").empty() || (rv = RegSetValueExA(key, "DLL64", 0, REG_SZ, reinterpret_cast<const BYTE*>(data.c_str()), data.size()+1)) != ERROR_SUCCESS) {
+	if (data.assign(path + L"office64.dll").empty()
+		|| (rv = RegSetValueExW(key, L"DLL64", 0, REG_SZ, reinterpret_cast<const BYTE*>(data.c_str()), (data.size()+1)*sizeof(data[0]))) != ERROR_SUCCESS) {
 		std::cerr << "Error: Could not write DLL64: " << rv << std::endl;
 	}
-	if (data.assign(buf + "kal.foma").empty() || (rv = RegSetValueExA(key, "LEX", 0, REG_SZ, reinterpret_cast<const BYTE*>(data.c_str()), data.size()+1)) != ERROR_SUCCESS
-		 || (rv = RegSetValueExA(key, "LEX64", 0, REG_SZ, reinterpret_cast<const BYTE*>(data.c_str()), data.size()+1)) != ERROR_SUCCESS) {
+	if (data.assign(path + L"speller.ini").empty()
+		|| (rv = RegSetValueExW(key, L"LEX", 0, REG_SZ, reinterpret_cast<const BYTE*>(data.c_str()), (data.size()+1)*sizeof(data[0]))) != ERROR_SUCCESS
+		|| (rv = RegSetValueExW(key, L"LEX64", 0, REG_SZ, reinterpret_cast<const BYTE*>(data.c_str()), (data.size()+1)*sizeof(data[0]))) != ERROR_SUCCESS) {
 		std::cerr << "Error: Could not write LEX and/or LEX64: " << rv << std::endl;
 	}
 
