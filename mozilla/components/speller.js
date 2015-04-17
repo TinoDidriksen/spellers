124d745e1e8c7d8726f91df1522fe4e7526c94b1
Tino Didriksen <mail@tinodidriksen.com>
2015-04-17 21:24:36 +0000

    Mozilla WIP
    git-svn-id: http://svn.tinodidriksen.com/tdc/oss/spellers@23 52bd5d93-d692-4aa0-9d28-25f7cffc0c32

--- mozilla/components/speller.js
+++ mozilla/components/speller.js
@@ -23,23 +23,22 @@ const mozISpellCheckingEngine = Components.interfaces.mozISpellCheckingEngine;
 const CLASS_ID = Components.ID("{GUID1}");
 const CLASS_NAME = "Spell checker for {LOCALES}";
 const CONTRACT_ID = "@tinodidriksen.com/speller;1";
+const MAX_SUGGS = 50;
 
 function Shim() {
 }
 
 Shim.prototype = {
-    libvoikko: null,
+    shim: null,
 
     data_loc: "",
 
     call_abi: ctypes.default_abi,
 
-    fn_voikko_init: null,
-    fn_voikko_terminate: null,
-    fn_voikko_spell_cstr: null,
-    fn_voikko_suggest_cstr: null,
-    fn_voikko_free_cstr_array: null,
-    fn_voikko_set_boolean_option: null,
+    fn_shim_init: null,
+    fn_shim_terminate: null,
+    fn_shim_spell_cstr: null,
+    fn_shim_suggest_cstr: null,
 
     init : function() {
         var runtime = Components.classes["@mozilla.org/xre/app-info;1"]
@@ -53,34 +52,34 @@ Shim.prototype = {
         // Detect shared library name to load
         //==================================================================
         if (abi == "Linux_x86-gcc3" || abi == "Linux_x86_64-gcc3") {
-            lib_name = "libvoikko.so.1";
+            lib_name = "shim.so.1";
         }
         else if (abi == "WINNT_x86-msvc" || abi == "WINNT_x86_64-msvc") {
-            lib_name = "libvoikko-1.dll";
+            lib_name = "shim-1.dll";
             this.call_abi = ctypes.winapi_abi;
         }
         else if (abi == "Darwin_x86_64-gcc3" || abi == "Darwin_x86-gcc3" || abi == "Darwin_ppc-gcc3") {
-            lib_name = "libvoikko.1.dylib";
+            lib_name = "shim.1.dylib";
         }
         else {
             throw "Unsupported ABI " + abi;
         }
 
-        // Try to locate libvoikko inside extension directory and
+        // Try to locate shim inside extension directory and
         // replace value
         var extension_dir = __LOCATION__.parent.parent;
-        var libvoikko_loc = extension_dir.clone();
-        libvoikko_loc.append("voikko");
-        libvoikko_loc.append(abi);
-        libvoikko_loc.append(lib_name);
-        if (libvoikko_loc.exists() && libvoikko_loc.isFile()) {
-            this.libvoikko = ctypes.open(libvoikko_loc.path);
-            aConsoleService.logStringMessage("{NAME}: loaded " + libvoikko_loc.path);
+        var shim_loc = extension_dir.clone();
+        shim_loc.append("voikko");
+        shim_loc.append(abi);
+        shim_loc.append(lib_name);
+        if (shim_loc.exists() && shim_loc.isFile()) {
+            this.shim = ctypes.open(shim_loc.path);
+            aConsoleService.logStringMessage("{NAME}: loaded " + shim_loc.path);
         }
         else {
-            this.libvoikko = ctypes.open(lib_name);
-            aConsoleService.logStringMessage("{NAME}: loaded system libvoikko");
-            aConsoleService.logStringMessage("{NAME}: libvoikko_loc = " + libvoikko_loc.path);
+            this.shim = ctypes.open(lib_name);
+            aConsoleService.logStringMessage("{NAME}: loaded system shim");
+            aConsoleService.logStringMessage("{NAME}: shim_loc = " + shim_loc.path);
         }
 
         var data_loc = extension_dir.clone();
@@ -94,82 +93,47 @@ Shim.prototype = {
             aConsoleService.logStringMessage("{NAME}: Found suomi-malaga data at " + this.data_loc);
         }
 
-        /* Detect used libvoikko version and output a message to Javascript console. */
-        var fn_voikkoGetVersion = this.libvoikko.declare(
-            "voikkoGetVersion",
+
+        //
+        // int shim_init();
+        //
+        this.fn_shim_init = this.shim.declare(
+            "shim_init",
             this.call_abi,
+            ctypes.int);
+
+        //
+        // void shim_terminate();
+        //
+        this.fn_shim_terminate = this.shim.declare(
+            "shim_terminate",
+            this.call_abi,
+            ctypes.void_t);
+
+        //
+        // int shim_is_valid_word(const char * word);
+        //
+        this.fn_shim_spell_cstr = this.shim.declare(
+            "shim_is_valid_word",
+            this.call_abi,
+            ctypes.int,
             ctypes.char.ptr);
 
-        var version_str = fn_voikkoGetVersion().readString();
-        aConsoleService.logStringMessage("{NAME}: Using libvoikko version " + version_str);
-
-
         //
-        // struct VoikkoHandle * voikkoInit(const char ** error, const char * langcode,
-        //                                  const char * path);
+        // const char ** shim_find_alternatives(const char * word, int suggs);
         //
-        this.fn_voikko_init = this.libvoikko.declare(
-            "voikkoInit",
+        this.fn_shim_suggest_cstr = this.shim.declare(
+            "shim_find_alternatives",
             this.call_abi,
-            ctypes.voidptr_t, // Return value: we are not interested what is inside
-            ctypes.char.ptr.ptr,
+            ctypes.char.ptr.array(MAX_SUGGS).ptr,
             ctypes.char.ptr,
-            ctypes.char.ptr);
-
-        //
-        // void voikkoTerminate(struct VoikkoHandle * handle);
-        //
-        this.fn_voikko_terminate = this.libvoikko.declare(
-            "voikkoTerminate",
-            this.call_abi,
-            ctypes.void_t,
-            ctypes.voidptr_t);
-
-        //
-        // int voikkoSpellCstr(struct VoikkoHandle * handle, const char * word);
-        //
-        this.fn_voikko_spell_cstr = this.libvoikko.declare(
-            "voikkoSpellCstr",
-            this.call_abi,
-            ctypes.int,
-            ctypes.voidptr_t,
-            ctypes.char.ptr);
-
-        //
-        // char ** voikkoSuggestCstr(struct VoikkoHandle * handle, const char * word);
-        //
-        this.fn_voikko_suggest_cstr = this.libvoikko.declare(
-            "voikkoSuggestCstr",
-            this.call_abi,
-            ctypes.char.ptr.array(50).ptr,
-            ctypes.voidptr_t,
-            ctypes.char.ptr);
-
-        //
-        // void voikkoFreeCstrArray(char ** cstrArray);
-        //
-        this.fn_voikko_free_cstr_array = this.libvoikko.declare(
-            "voikkoFreeCstrArray",
-            this.call_abi,
-            ctypes.void_t,
-            ctypes.char.ptr.array(50).ptr);
-
-        //
-        // int voikkoSetBooleanOption(struct VoikkoHandle * handle, int option, int value);
-        //
-        this.fn_voikko_set_boolean_option = this.libvoikko.declare(
-            "voikkoSetBooleanOption",
-            this.call_abi,
-            ctypes.int,
-            ctypes.voidptr_t,
-            ctypes.int,
             ctypes.int);
     },
 
     finalize : function() {
-        this.fn_voikko_init = null;
-        this.fn_voikko_terminate = null;
-        this.libvoikko.close();
+        this.fn_shim_init = null;
+        this.fn_shim_terminate = null;
+        this.shim.close();
     }
 };
 
@@ -178,48 +142,30 @@ var aConsoleService = Components.classes["@mozilla.org/consoleservice;1"].
 
 
 try {
-    var libvoikko = new Shim;
-    libvoikko.init();
+    var shim = new Shim;
+    shim.init();
 }
 catch (err) {
     Components.utils.reportError(err);
     throw err;
 }
 
-function VoikkoHandle(libvoikko) {
+
+function ShimHandle(shim) {
 }
 
-
-VoikkoHandle.prototype = {
+ShimHandle.prototype = {
     handle: null,
-    libvoikko: null,
-    lang_code: null,
+    shim: null,
 
-    open : function(libvoikko, lang_code) {
-        var message = ctypes.char.ptr(null);
-        var message_ptr = message.address();
-        var data_loc = null;
-
-        if (libvoikko.data_loc != "") {
-            data_loc = libvoikko.data_loc;
-        }
-
-        this.libvoikko = libvoikko;
-
-        this.handle = this.libvoikko.fn_voikko_init(
-            message_ptr,
-            "fi_FI",
-            data_loc);
-
-        this.libvoikko.fn_voikko_set_boolean_option(this.handle, VOIKKO_OPT_IGNORE_DOT, 1);
-        this.libvoikko.fn_voikko_set_boolean_option(this.handle, VOIKKO_OPT_IGNORE_NUMBERS, 1);
-        this.libvoikko.fn_voikko_set_boolean_option(this.handle, VOIKKO_OPT_IGNORE_UPPERCASE, 1);
-        this.libvoikko.fn_voikko_set_boolean_option(this.handle, VOIKKO_OPT_ACCEPT_MISSING_HYPHENS, 1);
+    open : function(shim) {
+        this.shim = shim;
+        this.handle = this.shim.fn_shim_init();
     },
 
     finalize : function() {
         if (handle) {
-            this.libvoikko.fn_voikko_terminate(this.handle);
+            this.shim.fn_shim_terminate(this.handle);
             this.handle = null;
         }
     }
@@ -228,8 +174,8 @@ VoikkoHandle.prototype = {
 
 function Speller() {
     try {
-        this.voikko_handle = new VoikkoHandle;
-        this.voikko_handle.open(libvoikko);
+        this.shim_handle = new ShimHandle;
+        this.shim_handle.open(shim);
     }
     catch (err) {
         Components.utils.reportError(err);
@@ -242,9 +188,10 @@ Speller.prototype = {
     classID: CLASS_ID,
     contractID: CONTRACT_ID,
 
-    data_loc : null,
-    voikko_handle : null,
-    mPersonalDictionary : null,
+    locales: {LOCALES_JSON},
+    shim_handle: null,
+    mPersonalDictionary: null,
+    mCurrentLocale: null,
 
     QueryInterface: XPCOMUtils.generateQI([mozISpellCheckingEngine, Components.interfaces.nsISupport]),
 
@@ -254,7 +201,7 @@ Speller.prototype = {
 
     set dictionary(dict) {
         if (dict != "fi_FI") {
-            throw "mozvoikko2: dictionary '" + dict + "' is not supported by this component (but may be supported by others)";
+            throw "{NAME}: dictionary '" + dict + "' is not supported by this component (but may be supported by others)";
         }
     },
 
@@ -267,11 +214,11 @@ Speller.prototype = {
     },
 
     get name() {
-        return "MozVoikko 2";
+        return "{NAME}";
     },
 
     get copyright() {
-        return "GPL v2";
+        return "GPL v3+";
     },
 
     get personalDictionary() {
@@ -282,47 +229,41 @@ Speller.prototype = {
         this.mPersonalDictionary = mPersonalDictionary;
     },
 
-    getDictionaryList : function(dicts, count) {
-        dicts.value = ["fi_FI"];
-        count.value = 1;
+    getDictionaryList: function(dicts, count) {
+        dicts.value = this.locales;
+        count.value = this.locales.length;
     },
 
-    check : function(word) {
-        if (word.length < 2) {
-            return true;
-        }
-        var result = libvoikko.fn_voikko_spell_cstr(this.voikko_handle.handle, word);
+    check: function(word) {
+        var result = shim.fn_shim_spell_cstr(word);
 
         if (result == 0 && this.mPersonalDictionary) {
             return this.mPersonalDictionary.check(word, "fi_FI");
         }
-        else {
-            return result != 0;
-        }
+		return result != 0;
     },
 
-    suggest : function(word, sugg, count) {
-        var i;
-        var tmp = libvoikko.fn_voikko_suggest_cstr(this.voikko_handle.handle, word);
+    suggest: function(word, sugg, count) {
+        var tmp = shim.fn_shim_suggest_cstr(word, MAX_SUGGS);
 
-        count.value = 0;
+		sugg.value = [];
+		count.value = 0;
 
-        if (!tmp.isNull()) {
-            sugg.value = [];
-            count.value = 0;
-
-            for (i = 0; i < 50 && !tmp.contents[i].isNull(); i++) {
-                sugg.value[i] = tmp.contents[i].readString();
-                count.value ++;
-            }
+        if (tmp.isNull()) {
+        	return;
         }
 
-        libvoikko.fn_voikko_free_cstr_array(tmp);
+		for (var i = 0; i < MAX_SUGGS && !tmp.contents[i].isNull(); ++i) {
+			sugg.value[i] = tmp.contents[i].readString();
+			++count.value;
+		}
     }
 }
 
 
-if (XPCOMUtils.generateNSGetFactory)
+if (XPCOMUtils.generateNSGetFactory) {
     var NSGetFactory = XPCOMUtils.generateNSGetFactory([Speller]);
-else
+}
+else {
     var NSGetModule = XPCOMUtils.generateNSGetModule([Speller]);
+}
---
+++ mozilla/native/shim.cpp
@@ -0,0 +1,335 @@
+#include <algorithm>
+#include <utility>
+#include <vector>
+#include <set>
+#include <locale>
+#include <codecvt>
+#include <string>
+#include <cstdio>
+#include <map>
+#include <cctype>
+#include <fstream>
+#include <stdexcept>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+
+#ifdef _WIN32
+	#include <windows.h>
+	#include <direct.h>
+	#define chdir _chdir
+
+	#ifdef DLL_EXPORTS
+		#define SPELLER_API __declspec(dllexport) WINAPI
+	#else
+		#define SPELLER_API __declspec(dllimport) WINAPI
+	#endif
+#else
+	#include <unistd.h>
+	#define SPELLER_API extern "C"
+#endif
+
+// Unnamed namespace to hide all these globals
+namespace {
+HANDLE g_hChildStd_IN_Rd = 0;
+HANDLE g_hChildStd_IN_Wr = 0;
+HANDLE g_hChildStd_OUT_Rd = 0;
+HANDLE g_hChildStd_OUT_Wr = 0;
+
+std::map<std::string, std::string> conf;
+std::unordered_set<std::string> valid_words;
+std::unordered_map<std::string, std::vector<std::string>> invalid_words;
+std::vector<const char*> rv_alts;
+std::string cbuffer;
+
+#ifndef NDEBUG
+std::ofstream debug("C:/Temp/Tino/debug-speller.txt", std::ios::binary);
+size_t debugd = 0;
+struct debugp {
+	std::string msg;
+
+	debugp(const std::string& msg) : msg(msg) {
+		debug << std::string(debugd, '\t') << '*' << msg << std::endl;
+		++debugd;
+	}
+
+	~debugp() {
+		--debugd;
+		debug << std::string(debugd, '\t') << '~' << msg << std::endl;
+	}
+
+	template<typename T>
+	void operator()(const T& t) {
+		debug << std::string(debugd, '\t');
+		for (size_t i=0 ; i<t.size() ; ++i) {
+			if (t[i] < 255) {
+				char c = static_cast<char>(t[i]);
+				debug.write(&c, 1);
+			}
+		}
+		debug << std::endl;
+	}
+
+	void operator()(size_t t) {
+		debug << std::string(debugd, '\t') << t << std::endl;
+	}
+};
+#else
+	#define debugp
+	#define p
+#endif
+
+inline std::string trim(std::string str) {
+	while (!str.empty() && std::isspace(str[str.size() - 1])) {
+		str.resize(str.size() - 1);
+	}
+	while (!str.empty() && std::isspace(str[0])) {
+		str.erase(str.begin());
+	}
+	return str;
+}
+
+void showLastError(const std::string& err) {
+	std::string msg = conf["NAME"] + " error location: ";
+	msg += err;
+	msg += "\n\n";
+	char *fmt = 0;
+	FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM|FORMAT_MESSAGE_ALLOCATE_BUFFER, 0, GetLastError(), MAKELANGID(LANG_NEUTRAL,SUBLANG_NEUTRAL), (LPSTR)&fmt, 0, 0);
+	msg += "GetLastError: ";
+	msg += fmt;
+	msg += '\n';
+	LocalFree(fmt);
+	MessageBoxA(0, msg.c_str(), "Speller Error", MB_OK|MB_ICONERROR);
+}
+
+bool checkValidWord(const std::string& word, size_t suggs = 0) {
+	debugp p("checkValidWord");
+	p(word);
+
+	if (valid_words.find(word) != valid_words.end()) {
+		return true;
+	}
+	if (invalid_words.find(word) != invalid_words.end()) {
+		return false;
+	}
+
+	cbuffer.resize(std::numeric_limits<size_t>::digits10+2);
+	cbuffer.resize(sprintf(&cbuffer[0], "%llu ", static_cast<unsigned long long>(suggs)));
+	cbuffer += word;
+	cbuffer += '\n';
+
+	DWORD bytes = 0, bytes_read = 0;
+	if (!WriteFile(g_hChildStd_IN_Wr, cbuffer.c_str(), cbuffer.size(), &bytes, 0) || bytes != cbuffer.size()) {
+		showLastError("checkValidWord WriteFile");
+		return false;
+	}
+	cbuffer.resize(1);
+	if (!ReadFile(g_hChildStd_OUT_Rd, &cbuffer[0], 1, &bytes_read, 0) || bytes_read != 1) {
+		showLastError("checkValidWord ReadFile 1");
+		return false;
+	}
+	if (!PeekNamedPipe(g_hChildStd_OUT_Rd, 0, 0, 0, &bytes, 0)) {
+		showLastError("checkValidWord PeekNamedPipe");
+		return false;
+	}
+	if (bytes) {
+		cbuffer.resize(1+bytes);
+		if (!ReadFile(g_hChildStd_OUT_Rd, &cbuffer[1], bytes, &bytes_read, 0) || bytes != bytes_read) {
+			showLastError("checkValidWord ReadFile 2");
+			return false;
+		}
+	}
+
+	cbuffer = trim(cbuffer);
+
+	if (cbuffer[0] == '*') {
+		valid_words.insert(word);
+		return true;
+	}
+
+	if (suggs == 0) {
+		return false;
+	}
+
+	std::vector<std::string>& alts = invalid_words[word];
+	alts.clear();
+
+	// This includes # for no alternatives and ! for error
+	if (cbuffer[0] != '&') {
+		return false;
+	}
+
+	size_t e = cbuffer.find("\t");
+	if (e == std::string::npos) {
+		return false;
+	}
+
+	size_t b = e + 1;
+	while ((e = cbuffer.find("\t", b)) != std::string::npos) {
+		alts.emplace_back(cbuffer.begin()+b, cbuffer.begin()+e);
+		b = e + 1;
+	}
+	alts.emplace_back(cbuffer.begin() + b, cbuffer.end());
+
+	return false;
+}
+}
+
+extern "C" int SPELLER_API shim_init() {
+	debugp p("shim_init");
+	SECURITY_ATTRIBUTES saAttr = {sizeof(saAttr), 0, true};
+
+	if (!CreatePipe(&g_hChildStd_OUT_Rd, &g_hChildStd_OUT_Wr, &saAttr, 0)) {
+		showLastError("shim_init CreatePipe 1");
+		return -__LINE__;
+	}
+	if (!SetHandleInformation(g_hChildStd_OUT_Rd, HANDLE_FLAG_INHERIT, 0)) {
+		showLastError("shim_init SetHandleInformation 1");
+		return -__LINE__;
+	}
+	if (!CreatePipe(&g_hChildStd_IN_Rd, &g_hChildStd_IN_Wr, &saAttr, 0)) {
+		showLastError("shim_init CreatePipe 2");
+		return -__LINE__;
+	}
+	if (!SetHandleInformation(g_hChildStd_IN_Wr, HANDLE_FLAG_INHERIT, 0)) {
+		showLastError("shim_init SetHandleInformation 2");
+		return -__LINE__;
+	}
+
+	PROCESS_INFORMATION piProcInfo = {0};
+	STARTUPINFOA siStartInfo = {sizeof(siStartInfo)};
+
+	siStartInfo.hStdError = g_hChildStd_OUT_Wr;
+	siStartInfo.hStdOutput = g_hChildStd_OUT_Wr;
+	siStartInfo.hStdInput = g_hChildStd_IN_Rd;
+	siStartInfo.dwFlags |= STARTF_USESTDHANDLES;
+
+	MEMORY_BASIC_INFORMATION mbiInfo = { 0 };
+	std::string path(MAX_PATH + 1, 0);
+	if (VirtualQuery(shim_init, &mbiInfo, sizeof(mbiInfo))) {
+		GetModuleFileNameA((HMODULE)(mbiInfo.AllocationBase), &path[0], MAX_PATH);
+	}
+	if (path[0] == 0) {
+		#ifdef _WIN64
+		GetModuleFileNameA(GetModuleHandleA("shim64.dl"), &path[0], MAX_PATH);
+		#else
+		GetModuleFileNameA(GetModuleHandleA("shim32.dl"), &path[0], MAX_PATH);
+		#endif
+	}
+	while (!path.empty() && path.back() != '/' && path.back() != '\\') {
+		path.pop_back();
+	}
+
+	std::string line;
+	std::ifstream inif(path + "speller.ini", std::ios::binary);
+	while (std::getline(inif, line)) {
+		line = trim(line);
+		if (line.empty() || line[0] == '#') {
+			continue;
+		}
+
+		size_t eqpos = line.find('=');
+		std::string key = trim(line.substr(0, eqpos));
+		std::string value = trim(line.substr(eqpos + 1));
+
+		conf[key] = value;
+	}
+
+	if (conf.empty() || !conf.count("ENGINE")) {
+		return -__LINE__;
+	}
+
+	path.append("backend");
+	if (chdir(path.c_str()) != 0) {
+		return -__LINE__;
+	}
+
+	std::string cmdline(conf["ENGINE"].begin(), conf["ENGINE"].end());
+	cmdline.append(1, 0);
+
+	BOOL bSuccess = CreateProcessA(0,
+		&cmdline[0],
+		0,
+		0,
+		TRUE,
+		CREATE_NO_WINDOW | BELOW_NORMAL_PRIORITY_CLASS,
+		0,
+		path.c_str(),
+		&siStartInfo,
+		&piProcInfo);
+
+	if (!bSuccess) {
+		std::string msg = conf["NAME"] + " could not start " + conf["ENGINE"] + "!\n\n";
+		char *fmt = 0;
+		FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM|FORMAT_MESSAGE_ALLOCATE_BUFFER, 0, GetLastError(), MAKELANGID(LANG_NEUTRAL,SUBLANG_NEUTRAL), (LPSTR)&fmt, 0, 0);
+		msg += "GetLastError: ";
+		msg += fmt;
+		msg += '\n';
+		LocalFree(fmt);
+		MessageBoxA(0, msg.c_str(), "Speller launch error", MB_OK|MB_ICONERROR);
+		return -__LINE__;
+	}
+	else {
+		CloseHandle(piProcInfo.hProcess);
+		CloseHandle(piProcInfo.hThread);
+	}
+
+	DWORD bytes = 0, bytes_read = 0;
+	cbuffer.resize(4);
+	if (!ReadFile(g_hChildStd_OUT_Rd, &cbuffer[0], 4, &bytes_read, 0) || bytes_read != 4) {
+		showLastError("shim_init ReadFile 1");
+		return -__LINE__;
+	}
+	if (!PeekNamedPipe(g_hChildStd_OUT_Rd, 0, 0, 0, &bytes, 0)) {
+		showLastError("shim_init PeekNamedPipe");
+		return -__LINE__;
+	}
+	if (bytes) {
+		cbuffer.resize(4 + bytes);
+		if (!ReadFile(g_hChildStd_OUT_Rd, &cbuffer[4], bytes, &bytes_read, 0) || bytes != bytes_read) {
+			showLastError("shim_init ReadFile 2");
+		return -__LINE__;
+		}
+	}
+
+	cbuffer = trim(cbuffer);
+	if (cbuffer != "@@ hfst-ospell-office is alive") {
+		return -__LINE__;
+	}
+
+	return 0;
+}
+
+extern "C" void SPELLER_API shim_terminate() {
+	debugp p("shim_terminate");
+	CloseHandle(g_hChildStd_IN_Rd);
+	CloseHandle(g_hChildStd_IN_Wr);
+	CloseHandle(g_hChildStd_OUT_Rd);
+	CloseHandle(g_hChildStd_OUT_Wr);
+}
+
+extern "C" int SPELLER_API shim_is_valid_word(const char *word) {
+	debugp p("shim_is_valid_word");
+	return static_cast<int>(checkValidWord(word));
+}
+
+typedef const char** ccharpp_t;
+extern "C" ccharpp_t SPELLER_API shim_find_alternatives(const char *word, int suggs) {
+	debugp p("shim_find_alternatives");
+	std::string str(word);
+
+	if (checkValidWord(str, suggs)) {
+		return 0;
+	}
+
+	rv_alts.clear();
+	const std::vector<std::string>& alts = invalid_words[str];
+	rv_alts.reserve(alts.size() + 1);
+
+	for (size_t i = 0; i < alts.size(); ++i) {
+		rv_alts.push_back(alts[i].c_str());
+	}
+	rv_alts.push_back(0);
+
+	return &rv_alts[0];
+}
