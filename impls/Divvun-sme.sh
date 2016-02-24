971768ec66663afc303e98e79fec271ac562a280
Tino Didriksen <mail@tinodidriksen.com>
2016-02-24 12:39:54 +0000

    lib -> share
    git-svn-id: http://svn.tinodidriksen.com/tdc/oss/spellers@91 52bd5d93-d692-4aa0-9d28-25f7cffc0c32

--- impls/Divvun-sme.sh
+++ impls/Divvun-sme.sh
@@ -8,5 +8,5 @@ wget 'http://apertium.projectjj.com/win32/nightly/hfst-ospell-latest.7z' -O hfst
 mv -v hfst-ospell/bin/* backend/
 wget 'http://apertium.projectjj.com/win32/nightly/data.php?deb=giella-sme' -O data.deb
 7z x data.deb
-tar -xf data.tar ./usr/lib/x86_64-linux-gnu/voikko/3/
-mv -v usr/lib/x86_64-linux-gnu/voikko/3/* backend/
+tar -xf data.tar ./usr/share/voikko/3/
+mv -v usr/share/voikko/3/* backend/
