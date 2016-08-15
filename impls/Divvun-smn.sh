fd7c2211d93bcfcb1b3c85721d17e1183b8003d0
Tino Didriksen <mail@tinodidriksen.com>
2016-08-15 14:35:20 +0000

    Add smn
    git-svn-id: http://svn.tinodidriksen.com/tdc/oss/spellers@93 52bd5d93-d692-4aa0-9d28-25f7cffc0c32

---
+++ impls/Divvun-smn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+mkdir -pv $1
+cd $1
+mkdir -pv backend
+wget 'http://apertium.projectjj.com/win32/nightly/hfst-ospell-latest.7z' -O hfst-ospell-latest.7z
+7z x hfst-ospell-latest.7z hfst-ospell/bin/
+mv -v hfst-ospell/bin/* backend/
+wget 'http://apertium.projectjj.com/win32/nightly/data.php?deb=giella-smn' -O data.deb
+7z x data.deb
+tar -xf data.tar ./usr/share/voikko/3/
+mv -v usr/share/voikko/3/* backend/
